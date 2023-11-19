0243edb6829f44750dabcf2aabad7e59f7dc3bda
ns.moskalev <nikmosyl@gmail.com>
2023-11-19 21:50:38 +0400

    implementation of views and functionality

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorView.layer.cornerRadius = 20
+        updateScreen()
     }
 
-
+    @IBAction func redSliderAction() {
+        updateScreen()
+    }
+    @IBAction func greenSliderAction() {
+        updateScreen()
+    }
+    @IBAction func blueSliderAction() {
+        updateScreen()
+    }
+    
+    private func updateScreen(){
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
