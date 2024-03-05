2f465aaf1f0a40fa0af70e617c527bcc4f7a6289
Nikita Moskalev <nikmosyl@gmail.com>
2024-03-05 22:33:09 +0400

    init SwiftUI

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by nikita on 05.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
